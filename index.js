cc7cbed280551fdd2f81680a7433ee9a54c17234
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-27 23:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ var funkyFunction = function() {
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
-theFunk = theFUNK()
+// NOte: you only need to modify the code below this line.
+var funk = funkyFunction()
+theFunk = theFunk()
